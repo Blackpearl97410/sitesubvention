54b05f9d93a0c1574083b1091bcd9d2f54f61586
Blackpearl97410 <lablackbox974@gmail.com>
2026-06-15 10:58:09 +0400

    Remove client space from conversion branch

--- database/turso/03-check.sql
+++
@@ -1,9 +0,0 @@
-select 'clients' as table_name, count(*) as row_count from clients
-union all
-select 'dossiers', count(*) from dossiers
-union all
-select 'dossier_documents', count(*) from dossier_documents
-union all
-select 'dossier_activity', count(*) from dossier_activity
-union all
-select 'client_access_tokens', count(*) from client_access_tokens;
